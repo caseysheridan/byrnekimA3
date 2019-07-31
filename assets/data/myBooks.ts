b44d9f65b6f673c6d7fafdd484ebc00a5d1f0dbb
caseysheridan <53196800+caseysheridan@users.noreply.github.com>
2019-07-31 15:13:30 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import { BookInfo } from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] = [
+        { sname : "The Giver", sauthor : "Lois Lowry", sgenre : "Young Adult Fiction", syear : 1993, spic : "giver.jpg" },
+        { sname: "House of Leaves", sauthor : "Mark Z. Danielewski", sgenre : "Horror", syear : 2000, spic : "House_of_leaves.jpg"},
+        { sname : "Embassytown", sauthor : "China Miéville", sgenre : "Science Fiction", syear : 2011, spic : "embassytown.jpg"},
+        { sname : "1984", sauthor : "George Orwell", sgenre : "Dystopian", syear : 1949, spic : "1984.jpg"}
+    ]
